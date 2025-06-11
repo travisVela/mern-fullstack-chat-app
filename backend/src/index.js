057b8a862d99dee2f2fa4551e15cd9b01eabb49e
Travis Vela <jtravisvela@gmail.com>
2025-06-11 15:17:33 -0500

    auth fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
+import { connectDB } from "./lib/db.js";
 
 import { app, server } from "./lib/socket.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 const BASE_URL =
-  import.meta.MODE === "devlopment" ? "http://localhost:5001" : "/";
+  import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
